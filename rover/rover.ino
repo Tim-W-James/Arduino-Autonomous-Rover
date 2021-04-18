78404b0318adf820319e60b5ae6f552f5dccdc1b
TimJ <tim.james9800@gmail.com>
2021-04-18 13:01:47 +1000

    Added reverse mode, Added level 1.1 and 1.2 test functions, Adjusted parameters

--- rover/rover.ino
+++ rover/rover.ino
@@ -10,18 +10,28 @@
 #define TRIG_PIN 8            // ultrasonic trigger (write)
 #define SERVO_PIN 11          // servo (write)
 
+// define the operation mode of the rover.
+// 0 - autonomous navigation for test levels 2-5
+// 1 - test level 1.1 (straight line and reverse)
+// 2 - test level 1.2 (square path)
+const int MODE = 0;
+
 // motor speed range 0-255
 const int MOTOR_SPEED = 100;
-const int TURN_SPEED = 255;   
+const int TURN_SPEED = 255; 
+// calibration to ensure the rover moves in a straight line
+const int MOTOR1_CALIBRATION = 0;
+const int MOTOR2_CALIBRATION = -5;
 // time it takes for the rover to turn 90 deg
-const int MOTOR_TURN_TIME = 440; 
+const int MOTOR_TURN_TIME = 410; 
 // time a rover will reverse before checking (ms)
-const int MOTOR_REVERSE_TIME = 5000; 
+const int MOTOR_REVERSE_TIME = 1500; 
+bool isReversing = false;
 
 // ultrasonic detects up to 336cm away - 5cm for error
 const long DISTANCE_LIMIT = 336 - 5;
 // allowed distance between rover and obstacles (cm)
-const long STOPPING_DISTANCE = 5;
+const long STOPPING_DISTANCE = 4.5;
 // allowed distance between rover and obstacles when turning (cm)
 const long TURN_CHECK_DISTANCE = 14;
 // frequency of ultrasonic polling (ms)
@@ -63,6 +73,22 @@ void setup()
 
 void loop()
 {
+  // operate based on mode setting
+  switch (MODE) {
+    case 1:
+      level_1_1();
+      break;
+    case 2:
+      level_1_2();
+      break;
+    default:
+      autonomousNavigation();
+      break;
+  }
+}
+
+// primary function for test levels 2 - 5
+void autonomousNavigation() {
   obstacleDistanceCM = checkDistance();
   
   // print distance to serial monitor
@@ -76,7 +102,7 @@ void loop()
   }
   
   // stop motor if obstacle gets too close
-  if (obstacleDistanceCM < STOPPING_DISTANCE) {
+  if (obstacleDistanceCM < STOPPING_DISTANCE && !isReversing) {
     motorsStop();
     Serial.println("Stopping...");
     
@@ -85,7 +111,7 @@ void loop()
     isObstacleRight = checkRight();
     // TODO 45 deg angles
     
-    if (!isObstacleLeft) {      // turn left if clear
+    if (!isObstacleLeft) {        // turn left if clear
       Serial.println("Turning left...");
       motorsRotateLeft();
     }
@@ -93,17 +119,47 @@ void loop()
       Serial.println("Turning right...");
       motorsRotateRight();
     }
-    else {              // begin reversing
+    else {                        // begin reversing
+      Serial.println("Dead end found, reversing...");
+      motorsBackward();
+      motorsStartCalibrated(MOTOR_SPEED);
+      isReversing = true; // enter reverse mode
+      delay(MOTOR_REVERSE_TIME);
+    }
+  }
+  // operate differently if reversing
+  // TODO improve backtracking mode
+  else if (isReversing) {
+    motorsStop();
+    Serial.println("Checking alternate paths...");
+    
+    // check left and right
+    isObstacleLeft = checkLeft();
+    isObstacleRight = checkRight();
+    // TODO 45 deg angles
+    
+    if (!isObstacleLeft) {      // turn left if clear
+      Serial.println("Alternate path found");
+      Serial.println("Turning left...");
+      motorsRotateLeft();
+      isReversing = false;
+    }
+    else if (!isObstacleRight) {  // turn right if clear
+      Serial.println("Alternate path found");
+      Serial.println("Turning right...");
+      motorsRotateRight();
+      isReversing = false;
+    }
+    else {
       Serial.println("Reversing...");
       motorsBackward();
       motorsStart(MOTOR_SPEED);
       delay(MOTOR_REVERSE_TIME);
-      // TODO backtracking mode
     }
   }
   else {
-    motorsStart(MOTOR_SPEED);
     motorsForward();
+    motorsStartCalibrated(MOTOR_SPEED);
     Serial.println("Moving forward...");    
   }
   
@@ -113,7 +169,7 @@ void loop()
 }
 
 /*
-  ULTRA-SONIC FUNCTIONS
+ * ULTRA-SONIC FUNCTIONS
 */
 
 long checkDistance() {
@@ -136,6 +192,9 @@ long checkDistance() {
 bool checkLeft(int deg, long maxDist) {
   servoLeft(deg);
   bool rtn = (checkDistance() < maxDist);
+  if (!rtn) {
+    Serial.println("Left is clear");
+  }
   servoReset(); // reset servo to neutral position
   return rtn;
 }
@@ -151,6 +210,9 @@ bool checkLeft() {
 bool checkRight(int deg, long maxDist) {
   servoRight(deg);
   bool rtn = (checkDistance() < maxDist);
+  if (!rtn) {
+    Serial.println("Right is clear");
+  }
   servoReset(); // reset servo to neutral position
   return rtn;
 }
@@ -162,7 +224,7 @@ bool checkRight() {
 }
 
 /*
-  MOTOR FUNCTIONS
+ * MOTOR FUNCTIONS
 */
 
 // turn the rover left a specified amount
@@ -172,6 +234,7 @@ void motorsRotateLeft(int dur) {
   delay(dur);
   // stop motors and revert to forward direction
   motorsStop();
+  delay(250);
   motorsForward();
 }
 void motorsRotateLeft() {
@@ -185,6 +248,7 @@ void motorsRotateRight(int dur) {
   delay(dur);
   // stop motors and revert to forward direction
   motorsStop();
+  delay(250);
   motorsForward();
 }
 void motorsRotateRight() {
@@ -220,6 +284,14 @@ void motorsStart(int speed) {
   analogWrite(MOTOR2_ENABLE_PIN, speed);
 }
 void motorsStart() {
+  motorsStart(MOTOR_SPEED);
+}
+void motorsStartCalibrated(int speed) {
+  // calibration to ensure the rover moves in a straight line
+  analogWrite(MOTOR1_ENABLE_PIN, speed+MOTOR1_CALIBRATION);
+  analogWrite(MOTOR2_ENABLE_PIN, speed+MOTOR2_CALIBRATION);
+}
+void motorsStartCalibrated() {
   motorsStart(MOTOR_SPEED);
 }
   
@@ -266,7 +338,7 @@ void invertMotor2() {
 }
 
 /*
-  SERVO FUNCTIONS
+ * SERVO FUNCTIONS
 */
 
 // reset servo to facing forward
@@ -292,3 +364,55 @@ void servoRight(int deg) {
 void servoRight() {
   servoRight(90);
 }
+
+/* 
+ *  TEST LEVEL DEMO FUNCTIONS
+*/
+
+void level_1_1() {
+  // move forward
+  motorsForward();
+  motorsStartCalibrated(MOTOR_SPEED);
+  delay(3000);
+  // stop 3 seconds
+  motorsStop();
+  delay(3000);
+  // reverse back to starting position
+  motorsBackward();
+  motorsStartCalibrated(MOTOR_SPEED);
+  delay(3000);
+  // finish
+  motorsStop();
+  delay(3000);
+}
+
+void level_1_2() {
+  // clockwise
+  for (int i = 0; i < 4; i++) {
+    motorsForward();
+    motorsStartCalibrated(MOTOR_SPEED);
+    delay(3000);
+    motorsStop();
+    delay(250);
+    motorsRotateRight();  
+    delay(250);
+  }  
+  delay(3000);
+  motorsRotateRight();  
+  delay(250);
+  // anti-clockwise
+  for (int i = 0; i < 4; i++) {
+    motorsForward();
+    motorsStartCalibrated(MOTOR_SPEED);
+    delay(3000);
+    motorsStop();    
+    delay(250);
+    motorsRotateLeft();  
+    delay(250);  
+  }
+  delay(3000);
+  motorsRotateLeft();    
+  delay(250);  
+}
+
+// levels 2 and onward can be tested with the autonomousNavigation() function
